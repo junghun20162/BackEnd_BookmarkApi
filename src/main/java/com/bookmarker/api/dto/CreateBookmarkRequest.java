7b72d6ac29f5fbe5d64798a8c98c3fc4897437fa
junghun <junghun20162@gmail.com>
2024-03-28 11:07:53 +0900

    bookmakr api initial commit

---
+++ src/main/java/com/bookmarker/api/dto/CreateBookmarkRequest.java
@@ -0,0 +1,15 @@
+package com.bookmarker.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class CreateBookmarkRequest {
+    @NotEmpty(message = "Title should not be empty")
+    private String title;
+    @NotBlank(message = "Url should not be empty")
+    private String url;
+}
