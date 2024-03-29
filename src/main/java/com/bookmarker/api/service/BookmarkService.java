2b81c789a9071d8def550a5ce344b587f853239c
junghun <junghun20162@gmail.com>
2024-03-29 17:54:44 +0900

    test

--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -25,7 +25,7 @@ public class BookmarkService {
     public BookmarksDTO<?> getBookmarks(Integer page) {
 
         int pageNo = page < 1 ? 0 : page - 1;
-        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "id");
+        Pageable pageable = PageRequest.of(pageNo, 5, Sort.Direction.DESC, "id");
         //Page<Bookmark> bookmarkPage = repository.findAll(pageable);
 
 //        Page<BookmarkDTO> bookmarkPage = repository.findAll(pageable)
@@ -39,7 +39,7 @@ public class BookmarkService {
     @Transactional(readOnly = true)
     public BookmarksDTO<?> searchBookmarks(String query, Integer page) {
         int pageNo = page < 1 ? 0 : page -1 ;
-        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "id");
+        Pageable pageable = PageRequest.of(pageNo, 5, Sort.Direction.DESC, "id");
 //        Page<BookmarkDTO> bookmarkPage = repository.searchBookmarks(query, pageable);
 //        Page<BookmarkDTO> bookmarkPage = repository.findByTitleContainsIgnoreCase(query, pageable);
         Page<BookmarkVM> bookmarkPage = repository.findByTitleContainsIgnoreCase(query, pageable);
